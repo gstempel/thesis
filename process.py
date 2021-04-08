904a682acb8bafce0599b648aea405f057ed2c91
gstempel <gstempel@princeton.edu>
2021-04-08 10:30:04 -0400

    add rest of notes

--- process.py
+++ process.py
@@ -1,11 +1,13 @@
 import os
+import re
 # Import packages for data visualization
 import plotly.offline as py
 import plotly.graph_objects as go
 import networkx as nx
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 G = nx.Graph()
+labeldict = {}
 
 class Node:
 
@@ -17,7 +19,24 @@ class Node:
 
     def update_lines(self):
         print("Updating lines")
-        
+
+regex = re.compile('see [also ]*note[s]* to[ lines]* [0-9]+[- ]*[0-9]*[ and]*[0-9]*')
+regex2 = re.compile('[1-9]+[- ]*[0-9]*')
+
+def parse_note(node):
+    f = open("text_docs/Notes/" + node.name, 'r')
+    for line in f:
+        s = regex.findall(line)
+        if s != []:
+            print(s)
+            for refs in s:
+                e = regex2.findall(refs)
+                print(e)
+                for n in e:
+                    new_node = Node(n + ".txt")
+                    G.add_edge(node, new_node)
+                    labeldict[new_node] = n
+                    
 
 # Take filename and return node object for file #
 def add_nodes():
@@ -30,10 +49,16 @@ def add_nodes():
         print(f)
         curr = Node(f)
         G.add_node(curr)
+        labeldict[curr] = (curr.name).replace(".txt", "")
+        parse_note(curr)
         continue
 
 def main():
     add_nodes()
+    print(G.number_of_nodes())
+    print(G.number_of_edges())
+    nx.draw(G, labels=labeldict, with_labels = True)
+    plt.show()
 
 if __name__ == "__main__":
     main()
